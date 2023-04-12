f9d1ecad0cf7e78eda46d025dc649176914c572d
VictorSantto <victor.sousa@codiub.com.br>
2023-04-12 16:16:57 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/com/victorsantos/workshopmogo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.victorsantos.workshopmogo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.victorsantos.workshopmogo.domain.User;
+import com.victorsantos.workshopmogo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+	
+}
