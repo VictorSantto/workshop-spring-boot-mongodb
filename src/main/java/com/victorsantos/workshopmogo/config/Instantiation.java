625184c9129ecf4523831ab69b0892bfa510137e
VictorSantto <victor.sousa@codiub.com.br>
2023-04-14 11:44:56 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/victorsantos/workshopmogo/config/Instantiation.java
+++ src/main/java/com/victorsantos/workshopmogo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.victorsantos.workshopmogo.domain.Post;
 import com.victorsantos.workshopmogo.domain.User;
+import com.victorsantos.workshopmogo.dto.AuthorDTO;
 import com.victorsantos.workshopmogo.repository.PostRepository;
 import com.victorsantos.workshopmogo.repository.UserRepository;
 
@@ -36,10 +37,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post mariaBrown = new Post(null,sdf.parse("21/03/2018") , "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post mariaBrown1 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post mariaBrown = new Post(null,sdf.parse("21/03/2018") , "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post mariaBrown1 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(mariaBrown, mariaBrown1));
 	}
 	
---
+++ src/main/java/com/victorsantos/workshopmogo/dto/AuthorDTO.java
@@ -0,0 +1,54 @@
+package com.victorsantos.workshopmogo.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.victorsantos.workshopmogo.domain.User;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+	}
+	
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		id = obj.getName();
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AuthorDTO other = (AuthorDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+}
